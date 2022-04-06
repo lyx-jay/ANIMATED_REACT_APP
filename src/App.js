2e7b05f175af1a31110de263e123ec082b0af96e
lyx-jay <1498781418@qq.com>
2022-04-06 20:56:26 +0800

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
-import "./styles/GlobalStyles.css"
+import "./styles/GlobalStyles.css";
+import "@fontsource/poppins";
+// import "@fontsource/poppins/500.css";
+// import "@fontsource/poppins/600.css";
+// import "@fontsource/poppins/700.css";
+// import "@fontsource/poppins/800.css";
 
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Header.css';
+import logo from '../../assets/images/logo.svg'
 
 
 class Header extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="navigation">
-          <div className="navMenu">
-            <nav>
-              <ul>
-                <li>home</li>
-                <li>servies</li>
-                <li>about</li>
-                <li>contact</li>
-              </ul>
-            </nav>
+      <header>
+        <div className="container">
+          <div className="navigation">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="navMenu">
+              <nav>
+                <ul>
+                  <li>home</li>
+                  <li>servies</li>
+                  <li>about</li>
+                  <li>contact</li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
+
     )
   }
 }
